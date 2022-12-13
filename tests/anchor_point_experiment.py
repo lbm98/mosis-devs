e66efa4624f97a17aea7e8e4e3e939d48fbaf471
lbm <lars.moons@student.uantwerpen.be>
2022-12-13 18:41:27 +0100

    initial commit

--- tests/anchor_point_experiment.py
+++ tests/anchor_point_experiment.py
@@ -1,68 +1,31 @@
 from pypdevs.simulator import Simulator
-from pypdevs.DEVS import AtomicDEVS, CoupledDEVS
+from pypdevs.DEVS import CoupledDEVS
 
 from models.vessels import CrudeOilTanker
 from models.anchor_point import AnchorPoint
 from models.control_tower import ControlTower
 
-
-# A simple generator that sends Vessel's every second
-class SimpleGenerator(AtomicDEVS):
-    def __init__(self, name):
-        super(SimpleGenerator, self).__init__(name)
-
-        # Sends PortEntryRequest's
-        self.out_vessel = self.addOutPort("out_vessel")
-
-        # Define the state
-        self.count = 0
-
-    def intTransition(self):
-        self.count += 1
-        return self.count
-
-    def timeAdvance(self):
-        return 1.0
-
-    def outputFnc(self):
-        vessel = CrudeOilTanker(
-            uid=self.count
-        )
-        return {self.out_vessel: vessel}
+from utils.simple_generator import SimpleGenerator
+from utils.simple_collector import SimpleCollector
 
 
-# A simple collector that receives Vessel's from AnchorPoint
-class SimpleCollector(AtomicDEVS):
-    def __init__(self, name):
-        super(SimpleCollector, self).__init__(name)
-
-        # Receives Vessel's
-        self.in_vessel = self.addInPort("in_vessel")
-
-        # Collects Vessel's
-        # Acts as the state
-        self.vessels = []
-
-    def extTransition(self, inputs):
-        if self.in_vessel in inputs:
-            vessel = inputs[self.in_vessel]
-            self.vessels.append(vessel)
-        return self.vessels
+def generate_vessels(count):
+    return CrudeOilTanker(uid=count)
 
 
 class CoupledAnchorPoint(CoupledDEVS):
     def __init__(self, name):
         super(CoupledAnchorPoint, self).__init__(name)
 
-        self.simple_generator = self.addSubModel(SimpleGenerator("simple_generator"))
+        self.simple_generator = self.addSubModel(SimpleGenerator("simple_generator", item_generator=generate_vessels))
         self.anchor_point = self.addSubModel(AnchorPoint("anchor_point"))
         self.control_tower = self.addSubModel(ControlTower("control_tower"))
         self.simple_collector = self.addSubModel(SimpleCollector("simple_collector"))
 
-        self.connectPorts(self.simple_generator.out_vessel, self.anchor_point.in_vessel)
+        self.connectPorts(self.simple_generator.out_item, self.anchor_point.in_vessel)
         self.connectPorts(self.anchor_point.out_port_entry_request, self.control_tower.in_port_entry_request)
         self.connectPorts(self.control_tower.out_port_entry_permission, self.anchor_point.in_port_entry_permission)
-        self.connectPorts(self.anchor_point.out_vessel, self.simple_collector.in_vessel)
+        self.connectPorts(self.anchor_point.out_vessel, self.simple_collector.in_item)
 
 
 def test_anchor_point():
@@ -73,13 +36,13 @@ def test_anchor_point():
     sim.setClassicDEVS()
     sim.simulate()
 
-    result = system.simple_collector.vessels
+    vessels = system.simple_collector.items
 
-    assert result[0].uid == 0 and result[0].destination_dock == '1'
-    assert result[1].uid == 1 and result[1].destination_dock == '2'
-    assert result[2].uid == 2 and result[2].destination_dock == '3'
-    assert result[3].uid == 3 and result[3].destination_dock == '4'
-    assert len(result) == 8
+    assert vessels[0].uid == 0 and vessels[0].destination_dock == '1'
+    assert vessels[1].uid == 1 and vessels[1].destination_dock == '2'
+    assert vessels[2].uid == 2 and vessels[2].destination_dock == '3'
+    assert vessels[3].uid == 3 and vessels[3].destination_dock == '4'
+    assert len(vessels) == 8
 
 
 if __name__ == "__main__":
--- tests/control_tower_experiment.py
+++ tests/control_tower_experiment.py
@@ -1,64 +1,30 @@
 from pypdevs.simulator import Simulator
-from pypdevs.DEVS import AtomicDEVS, CoupledDEVS
+from pypdevs.DEVS import CoupledDEVS
 
 from models.messages import PortEntryRequest
 from models.control_tower import ControlTower
 
-
-# A simple generator that sends PortEntryRequest every second
-class SimpleGenerator(AtomicDEVS):
-    def __init__(self, name):
-        super(SimpleGenerator, self).__init__(name)
-
-        # Sends PortEntryRequest's
-        self.out_port_entry_request = self.addOutPort("out_port_entry_request")
-
-        # Define the state
-        self.count = 0
-
-    def intTransition(self):
-        self.count += 1
-        return self.count
-
-    def timeAdvance(self):
-        return 1.0
-
-    def outputFnc(self):
-        port_entry_request = PortEntryRequest(
-            vessel_uid=self.count
-        )
-        return {self.out_port_entry_request: port_entry_request}
+from utils.simple_collector import SimpleCollector
+from utils.simple_generator import SimpleGenerator
 
 
-# A simple collector that receives PortEntryPermission's from ControlTower
-class SimpleCollector(AtomicDEVS):
-    def __init__(self, name):
-        super(SimpleCollector, self).__init__(name)
-
-        # Receives PortEntryPermission's
-        self.in_port_entry_permission = self.addInPort("in_port_entry_permission")
-
-        # Collects PortEntryPermission's
-        # Acts as the state
-        self.port_entry_permissions = []
-
-    def extTransition(self, inputs):
-        if self.in_port_entry_permission in inputs:
-            port_entry_permission = inputs[self.in_port_entry_permission]
-            self.port_entry_permissions.append(port_entry_permission)
-        return self.port_entry_permissions
+def generate_port_entry_requests(count):
+    return PortEntryRequest(
+        vessel_uid=count
+    )
 
 
 class CoupledControlTower(CoupledDEVS):
     def __init__(self, name):
         super(CoupledControlTower, self).__init__(name)
 
-        self.simple_generator = self.addSubModel(SimpleGenerator("simple_generator"))
+        self.simple_generator = self.addSubModel(
+            SimpleGenerator("simple_generator", item_generator=generate_port_entry_requests))
         self.control_tower = self.addSubModel(ControlTower("control_tower"))
         self.simple_collector = self.addSubModel(SimpleCollector("simple_collector"))
 
-        self.connectPorts(self.simple_generator.out_port_entry_request, self.control_tower.in_port_entry_request)
-        self.connectPorts(self.control_tower.out_port_entry_permission, self.simple_collector.in_port_entry_permission)
+        self.connectPorts(self.simple_generator.out_item, self.control_tower.in_port_entry_request)
+        self.connectPorts(self.control_tower.out_port_entry_permission, self.simple_collector.in_item)
 
 
 def test_control_tower():
@@ -69,15 +35,14 @@ def test_control_tower():
     sim.setClassicDEVS()
     sim.simulate()
 
-    result = system.simple_collector.port_entry_permissions
+    perm = system.simple_collector.items
 
-    assert result[0].vessel_uid == 0 and result[0].avl_dock == '1'
-    assert result[1].vessel_uid == 1 and result[1].avl_dock == '2'
-    assert result[2].vessel_uid == 2 and result[2].avl_dock == '3'
-    assert result[3].vessel_uid == 3 and result[3].avl_dock == '4'
-    assert len(result) == 8
+    assert perm[0].vessel_uid == 0 and perm[0].avl_dock == '1'
+    assert perm[1].vessel_uid == 1 and perm[1].avl_dock == '2'
+    assert perm[2].vessel_uid == 2 and perm[2].avl_dock == '3'
+    assert perm[3].vessel_uid == 3 and perm[3].avl_dock == '4'
+    assert len(perm) == 8
 
 
 if __name__ == "__main__":
     test_control_tower()
-
