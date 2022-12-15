8bcc33ae8b0b4b191d8d3765402ffbad424bd99a
lbm <lars.moons@student.uantwerpen.be>
2022-12-15 21:23:21 +0100

    cleanup

--- tests/run_all.py
+++ tests/run_all.py
@@ -3,8 +3,10 @@ import os
 
 if __name__ == "__main__":
     os.system('python anchor_point_experiment.py')
-    os.system('python old_canal_experiment.py')
+    os.system('python canal_experiment.py')
     os.system('python confluence_experiment.py')
     os.system('python control_tower_experiment.py')
     os.system('python generator_experiment.py')
+    os.system('python uni_canal_experiment.py')
     os.system('python uni_waterway_experiment.py')
+    os.system('python waterway_experiment.py')
